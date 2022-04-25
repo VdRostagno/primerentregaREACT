3dc6a3f98c6e9b95ac85a3edef061950865f769f
Virginia Dana Rostagno <95722444+VdRostagno@users.noreply.github.com>
2022-04-25 14:27:16 -0300

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar"
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__titlo">E-Commerce</h1>
+      <NavBar/>
+    </header>
+  )
+}
+
+export default Header;
+
