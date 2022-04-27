e4186a9cc5593598a6808569b76adf34fb9913a2
Virginia Dana Rostagno <95722444+VdRostagno@users.noreply.github.com>
2022-04-27 06:06:07 -0300

    Add files via upload

---
+++ src/Items.js
@@ -0,0 +1,27 @@
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+
+
+function Items({id,nombre,imagen,stock,precio}){
+    return ( 
+       
+                    <Col >
+                        <Card >
+                            <Card.Id className="id-productos">{id}</Card.Id>
+                            <Card.Img className="imagen-productos" variant="top" src={imagen} />
+                            <Card.Body className="cuerpo">
+                            <Card.Title className="nombre-productos">{nombre}</Card.Title>
+                            <p> Stock Disponible: {stock}</p>
+                            <Card.Precio className="precio-productos">{precio}</Card.Precio>
+                            
+                             <Button variant="outline-dark" className="boton-detalle" >Ver detalles</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                
+    )
+}
+export default Items
+
