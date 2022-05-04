db34fd7243437660f833281f708ad4a2edb77053
Virginia Dana Rostagno <95722444+VdRostagno@users.noreply.github.com>
2022-05-04 11:48:19 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+
+import Header from "./Header"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+import ItemListContainer from "./ItemListContainer"
+
+const App = () => {   
+
+  return (    
+  
+    <>      
+  
+  <NavBar/>
+  
+   <ItemListContainer greeting={"Bienvenido a mi E-Commerce"} />    
+   
+  <Footer/>      
+  
+  </> 
+  
+    )}
+
+    export default App
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import ItemListContainer from "./ItemListContainer"
+
+const Main = () => {
+
+  return (
+    <main>
+      <h1>Hola, bienvenido </h1>
+      <ItemListContainer/>
+    </main>
+  )
+}
+
+export default Main
