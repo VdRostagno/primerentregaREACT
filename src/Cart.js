2119bc09d61162ff628bd5b102225137adf9e0ae
Virginia Dana Rostagno <95722444+VdRostagno@users.noreply.github.com>
2022-05-13 11:34:49 -0300

    Add files via upload

---
+++ src/Cart.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import Table from "react-bootstrap/Table"
+import Container from "react-bootstrap/Container"
+
+export  default function Cart() {
+
+    return(
+        <>
+       <h1>ESTAS EN EL CARRITO</h1>
+      <Container>  
+       <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                
+                <th>Cantidad</th>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Total</th>
+                </tr>
+            </thead>
+        </Table>
+      </Container> 
+                    <Form className="d-flex">
+                        <Button variant="outline-danger" >X</Button>
+                        <Button variant="outline-success">Finalizar Comprar</Button>
+                    </Form>
+        </>
+    )
+}
